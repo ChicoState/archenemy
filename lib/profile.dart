062ce315fbac3182ee1aee9cc0944ce119ec5814
Elias Boyd <eboyd@mail.csuchico.edu>
2025-02-17 13:47:46 -0800

    groundwork for profile editing

--- lib/profile.dart
+++ lib/profile.dart
@@ -12,23 +12,63 @@ class _ProfileState extends State<Profile> {
     //return Placeholder();
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text('My Profile'),
-        ),
-        body: Center(
-          child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(style: TextStyle(fontSize: 64), 'Jane Doe'),
-                    Expanded(child: Placeholder()),
-                    Text('Diskiles....'),
-                    Text('chocolate'),
-                    Text('ur mom'),
-                    Text('Chevrolet')
-                  ])),
-        ));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('My Profile'),
+      ),
+      body: Center(
+        child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(style: TextStyle(fontSize: 64), 'Jane Doe'),
+                  Expanded(child: Placeholder()),
+                  Text('Diskiles....'),
+                  Text('chocolate'),
+                  Text('ur mom'),
+                  Text('Chevrolet')
+                ])),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => EditProfile()));
+        },
+        child: const Icon(Icons.manage_accounts),
+      ),
+    );
+  }
+}
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('My Profile'),
+      ),
+      body: Center(
+        child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter your name',
+                    ),
+                  ),
+                ])),
+      ),
+    );
   }
 }
