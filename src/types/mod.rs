b6ba4fc491e5fc1ddbcd78f34d008cd6ce90dc0f
Yuvinscria Werdxz <werdxz@werdxz.info>
2025-02-24 14:46:12 -0800

    added storage apis

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,15 +2,21 @@ use axum::{http::StatusCode, response::IntoResponse};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
+#[non_exhaustive]
 pub enum Error {
     Unauthenticated,
+    S3 { msg: String },
+    MultipartParse { msg: String },
+    Unknown,
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> axum::response::Response {
         let status_code = match self {
             Self::Unauthenticated => StatusCode::UNAUTHORIZED,
-            // TODO: Add a new branch for every error variant
+            Self::S3 { msg: _ } => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::MultipartParse { msg: _ } => StatusCode::BAD_REQUEST,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
         };
 
         (status_code, self).into_response()
