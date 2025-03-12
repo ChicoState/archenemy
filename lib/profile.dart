0c329b738f76087adb223510e8c205a5577b8f25
Elias Boyd <eboyd@mail.csuchico.edu>
2025-03-12 13:54:05 -0700

    fixing conflits with main

--- lib/profile.dart
+++ lib/profile.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'log.dart';
 
-
 class Profile {
   String name;
   DateTime birthDate;
@@ -22,11 +21,7 @@ class Profile {
     try {
       var map = json.decode(raw);
       return Profile(
-				map.name,
-				DateTime.parse(map.birthDate),
-				map.bio,
-				map.interests
-			);
+          map.name, DateTime.parse(map.birthDate), map.bio, map.interests);
     } catch (err) {
       log.e("Profile JSON parsing error: $err");
       rethrow;
@@ -38,67 +33,68 @@ class Profile {
 }
 
 class ProfileView extends StatelessWidget {
-	
-	final Profile profile;
-	final bool editable; // doesn't work yet
-	// not sure whether profile editing should happen on another page or not
-	
-  const ProfileView(this.profile, {
-		super.key,
-		this.editable = false
-	});
-  
-	@override
+  final Profile profile;
+  final bool editable; // doesn't work yet
+  // not sure whether profile editing should happen on another page or not
+
+  const ProfileView(this.profile, {super.key, this.editable = false});
+
+  @override
   Widget build(BuildContext context) {
     //return Placeholder();
-		
-		Widget interestsView(List<String> interests) {
-			return Text.rich(
-				TextSpan(
-					children: interests.map((interest) {
-						return WidgetSpan(
-							child: Card(
-								margin: EdgeInsets.fromLTRB(3.0, 3.0, 3.0, 3.0),
-								color: Colors.grey[400],//Theme.of(context).cardColor,
-								shape: RoundedRectangleBorder(
-									borderRadius: BorderRadius.all(Radius.circular(4.0))
-								),
-								child: Padding(
-									padding: EdgeInsets.fromLTRB(5.0, 3.0, 5.0, 3.0),
-									child: Text(interest)
-								),
-							)
-						);
-					}).toList()
-				)
-			);
-		}
-		
-		final children = [
-			Text(style: TextStyle(fontSize: 24), profile.name),
-			SizedBox(height: 160, child: Placeholder()),
-			interestsView(profile.interests),
-			Text(profile.bio),
-			SizedBox(height: 160, child: Placeholder()),
-			SizedBox(height: 160, child: Placeholder()),
-		];
-		
-		
-		// Unbelievably, this is the easiest way I could find to do this
-		List<Widget> spacedChildren = [];
-		for (Widget child in children) {
-			spacedChildren.add(child);
-			spacedChildren.add(SizedBox(height: 10));
-		}
-		
-		return Padding(
-			padding: EdgeInsets.all(16.0),
-			child: ListView(
-				//mainAxisAlignment: MainAxisAlignment.center,
-				children: spacedChildren
-			)
-		);
-		
+
+    Widget interestsView(List<String> interests) {
+      return Text.rich(TextSpan(
+          children: interests.map((interest) {
+        return WidgetSpan(
+            child: Card(
+          margin: EdgeInsets.fromLTRB(3.0, 3.0, 3.0, 3.0),
+          color: Colors.grey[400], //Theme.of(context).cardColor,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(4.0))),
+          child: Padding(
+              padding: EdgeInsets.fromLTRB(5.0, 3.0, 5.0, 3.0),
+              child: Text(interest)),
+        ));
+      }).toList()));
+    }
+
+    final children = [
+      Row(
+        children: [
+          Text(style: TextStyle(fontSize: 24), profile.name),
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => EditProfile(
+                              myProfile: profile,
+                            )));
+              },
+              icon: Icon(Icons.menu))
+        ],
+      ),
+      SizedBox(height: 160, child: Placeholder()),
+      interestsView(profile.interests),
+      Text(profile.bio),
+      SizedBox(height: 160, child: Placeholder()),
+      SizedBox(height: 160, child: Placeholder()),
+    ];
+
+    // Unbelievably, this is the easiest way I could find to do this
+    List<Widget> spacedChildren = [];
+    for (Widget child in children) {
+      spacedChildren.add(child);
+      spacedChildren.add(SizedBox(height: 10));
+    }
+
+    return Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: spacedChildren));
+
     /*return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -149,6 +145,50 @@ class ProfileView extends StatelessWidget {
   }
 }
 
+class ExplorePage extends StatefulWidget {
+  List<Profile> profiles;
+  ExplorePage(this.profiles, {super.key});
+
+  @override
+  createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+  @override
+  build(BuildContext context) {
+    final profiles = widget.profiles;
+    final profileView = profiles.isEmpty
+        ? Center(child: Text("No more profiles!"))
+        : ProfileView(profiles.last);
+
+    return Stack(children: [
+      profileView,
+      Positioned(
+          bottom: 10.0,
+          left: 10.0,
+          child: IconButton.filled(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                log.d("Disiked!");
+                setState(() {
+                  widget.profiles.removeLast();
+                });
+              })),
+      Positioned(
+        bottom: 10.0,
+        right: 10.0,
+        child: IconButton.filled(
+            icon: Icon(Icons.check),
+            onPressed: () {
+              log.d("Liked!");
+              setState(() {
+                widget.profiles.removeLast();
+              });
+            }),
+      )
+    ]);
+  }
+}
 
 ////////////////////////////////////////////////////
 /// Use: use navigator.push and pass a profile class.
