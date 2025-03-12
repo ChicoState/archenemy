7b159b61e4175acfc45470ab8fe17ca028cb13a8
Elias Boyd <eboyd@mail.csuchico.edu>
2025-03-11 20:48:03 -0700

    Fixed profile editing issues, profile editing is fully functional

--- lib/profile.dart
+++ lib/profile.dart
@@ -80,7 +80,11 @@ class _ProfileBoardState extends State<ProfileBoard> {
               MaterialPageRoute(
                   builder: (context) => EditProfile(
                         myProfile: widget.myProfile,
-                      )));
+                      ))).then((_) {
+            setState(() {
+              widget.myProfile;
+            });
+          });
         },
         child: const Icon(Icons.manage_accounts),
       ),
@@ -88,6 +92,20 @@ class _ProfileBoardState extends State<ProfileBoard> {
   }
 }
 
+////////////////////////////////////////////////////
+/// Use: use navigator.push and pass a profile class.
+/// then put :
+///
+/// .then((_) {
+///   setState(() {
+///     widget.myProfile;
+///   });
+/// }
+/// immediately after the Navigator.push argument
+/// the profile will be edited in the next page
+/// once the data is saved and pop'd off the widget tree
+/// the profile data will be updated on the current page
+///////////////////////////////////////////////////////
 class EditProfile extends StatefulWidget {
   const EditProfile({super.key, required this.myProfile});
   final Profile myProfile;
@@ -99,6 +117,89 @@ class _EditProfileState extends State<EditProfile> {
   DateTime? selectedDate = DateTime.now();
   String? enteredName = "name";
   String? enteredBio = "bio";
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController bioController = TextEditingController();
+  final GlobalKey<FormState> nameFormKey = GlobalKey<FormState>();
+  String? forceErrorText;
+  bool isLoading = false;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    bioController.dispose();
+    super.dispose();
+  }
+
+  String? validator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'This field is required';
+    }
+    if (int.tryParse(value[0]) != null) {
+      return 'Name must not start with a number';
+    }
+    return null;
+  }
+
+  String? bioValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'This field is required';
+    }
+    return null;
+  }
+
+  void onChanged(String value) {
+    // Nullify forceErrorText if the input changed.
+    if (forceErrorText != null) {
+      setState(() {
+        forceErrorText = null;
+      });
+    }
+  }
+
+  Future<void> onSave() async {
+    // Providing a default value in case this was called on the
+    // first frame, the [fromKey.currentState] will be null.
+    final bool isValid = nameFormKey.currentState?.validate() ?? false;
+    if (!isValid) {
+      return;
+    }
+
+    setState(() => isLoading = true);
+    final String? errorText =
+        await validateUsernameFromServer(nameController.text);
+
+    if (context.mounted) {
+      setState(() => isLoading = false);
+      if (errorText != null) {
+        setState(() {
+          forceErrorText = errorText;
+        });
+      }
+
+      setState(() {
+        // widget.myProfile.update(nameController.text,
+        //     selectedDate ?? DateTime.now(), bioController.text);
+        widget.myProfile.name = nameController.text;
+        widget.myProfile.birthDate = selectedDate ?? DateTime.now();
+        widget.myProfile.bio = bioController.text;
+        //print("Saved");
+      });
+    }
+  }
+
+  Future<String?> validateUsernameFromServer(String username) async {
+    final Set<String> takenUsernames = <String>{'jack', 'alex'};
+
+    await Future<void>.delayed(Duration(seconds: 1));
+
+    final bool isValid = !takenUsernames.contains(username);
+    if (isValid) {
+      return null;
+    }
+
+    return 'Username $username is already taken';
+  }
 
   Future<void> _selectDate() async {
     final DateTime? pickedDate = await showDatePicker(
@@ -123,90 +224,50 @@ class _EditProfileState extends State<EditProfile> {
       body: Center(
         child: Padding(
             padding: EdgeInsets.all(16.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Enter your name',
-                    ),
-                    onSaved: (String? val) {
-                      setState(() {
-                        enteredName = val;
-                      });
-                    },
-                  ),
-                  TextFormField(
-                    minLines: 1,
-                    maxLines: 10,
-                    maxLength: 1000,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'What really grinds your gears',
-                    ),
-                    onSaved: (String? val) {
-                      enteredBio = val;
-                    },
-                  ),
-                  Text(
-                    selectedDate != null
-                        ? '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}'
-                        : 'No date selected',
-                  ),
-                  TextButton(
-                      onPressed: _selectDate, child: const Text("Birthday")),
-                  TextButton(
-                    onPressed: () {
-                      if (enteredName == null) {
-                        final nameSnackBar = SnackBar(
-                          content: const Text('Please enter your name'),
-                          action: SnackBarAction(
-                            label: 'Undo',
-                            onPressed: () {
-                              // Some code to undo the change.
-                            },
-                          ),
-                        );
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(nameSnackBar);
-                      } else if (enteredBio == null) {
-                        final bioSnackBar = SnackBar(
-                          content:
-                              const Text('please enter your pio information'),
-                          action: SnackBarAction(
-                            label: 'Undo',
-                            onPressed: () {
-                              // Some code to undo the change.
-                            },
-                          ),
-                        );
-                        ScaffoldMessenger.of(context).showSnackBar(bioSnackBar);
-                      } else if (selectedDate == null) {
-                        final dateSnackBar = SnackBar(
-                          content: const Text('Please select your birthday'),
-                          action: SnackBarAction(
-                            label: 'Undo',
-                            onPressed: () {
-                              // Some code to undo the change.
-                            },
-                          ),
-                        );
-
-                        // Find the ScaffoldMessenger in the widget tree
-                        // and use it to show a SnackBar.
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(dateSnackBar);
-                      } else {
-                        widget.myProfile.update(
-                            enteredName ?? "Jane doe",
-                            selectedDate ?? DateTime.now(),
-                            enteredBio ?? "bio");
-                      }
-                    },
-                    child: Text('Save'),
-                  )
-                ])),
+            child: Form(
+                key: nameFormKey,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        forceErrorText: forceErrorText,
+                        controller: nameController,
+                        validator: validator,
+                        onChanged: onChanged,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Enter your name',
+                        ),
+                      ),
+                      TextFormField(
+                        minLines: 1,
+                        maxLines: 10,
+                        maxLength: 1000,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'What really grinds your gears',
+                        ),
+                        forceErrorText: forceErrorText,
+                        controller: bioController,
+                        validator: bioValidator,
+                        onChanged: onChanged,
+                      ),
+                      Text(
+                        selectedDate != null
+                            ? '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}'
+                            : 'No date selected',
+                      ),
+                      TextButton(
+                          onPressed: _selectDate,
+                          child: const Text("Birthday")),
+                      if (isLoading)
+                        const CircularProgressIndicator()
+                      else
+                        TextButton(
+                          onPressed: onSave,
+                          child: Text('Save'),
+                        )
+                    ]))),
       ),
     );
   }
